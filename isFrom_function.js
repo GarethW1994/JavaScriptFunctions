a29db0a0fc180fb1a329e1fef100b367e75e5d58
Gareth <williams.gareth0@gmail.com>
2016-09-28 16:37:13 +0200

    Save Changes

--- isFrom_function.js
+++ isFrom_function.js
@@ -1,6 +1,12 @@
 
-var isFrom = function(location) {
-  
+var isFrom = function(regNum, location) {
+  var isFromPaarl = location.startsWith('CJ');
+  var isFromBellville = location.startsWith('CY');
+  var isFromCapeTown = location.startsWith('CA');
+
+  console.log('From Paarl: ' + isFromPaarl);
+  console.log('From Bellville: ' + isFromBellville);
+  console.log('From Cape Town: ' + isFromCapeTown);
 }
 
-isFrom('CJ');
+isFrom('CJ 98912', 'CJ');
---
+++ regCheck_Function.js
@@ -0,0 +1,7 @@
+var regCheck = function(regNum, location) {
+  var isGP = regNum.includes(location);
+
+  console.log(isGP);
+}
+
+regCheck('DV 23 NB GP', 'GP');
